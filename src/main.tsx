faa203dcda5371261354ab4dd904088a08be5e4c
Артём Одийко <odiykoartem@gmail.com>
2022-08-18 17:33:38 +0300

    Added scroll to top behaviour

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { ScrollToTop } from "./components/ScrollToTop";
+
 import "react-phone-input-2/lib/style.css";
 import "./assets/css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
+			<ScrollToTop />
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
