adf3a65d75fee882efd5458082d13ad31b4046e1
Артём Одийко <odiykoartem@gmail.com>
2022-08-18 18:01:16 +0300

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
+import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/Signup";
+import NotFound from "./pages/NotFound";
 
-const App = () => {
-	return <Signup />;
+const App: React.FC = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Dashboard />} />
+			<Route path="signup" element={<Signup />} />
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	);
 };
 
 export default App;
