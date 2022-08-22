c04f9e3b42a35187d8b4bc388f2d7ead3bcd527f
Артём Одийко <odiykoartem@gmail.com>
2022-08-22 18:12:59 +0300

    Added new routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import { Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/Signup";
+import Settings from "./pages/Settings";
 import NotFound from "./pages/NotFound";
+import SettingsName from "./pages/SettingsName";
+import SettingsAccount from "./pages/SettingsAccount";
+import SettingsNotification from "./pages/SettingsNotification";
 
 const App: React.FC = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Dashboard />} />
 			<Route path="signup" element={<Signup />} />
+			<Route path="settings" element={<Settings />} />
+			<Route path="settings/name" element={<SettingsName />} />
+			<Route path="settings/account" element={<SettingsAccount />} />
+			<Route path="settings/notifications" element={<SettingsNotification />} />
 			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
