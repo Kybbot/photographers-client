75a9dff5ed4e509d1ca3237e9c5d9a839c8d6144
Артём Одийко <odiykoartem@gmail.com>
2022-09-07 18:50:22 +0300

    Added props

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -1,36 +1,41 @@
 import React from "react";
 
-export const Lightbox: React.FC = () => {
+type LightboxProps = {
+	currentPrint: string;
+	closeModal: () => void;
+};
+
+export const Lightbox: React.FC<LightboxProps> = ({ currentPrint, closeModal }) => {
 	return (
 		<section className="lightbox">
 			<div className="lightbox__container">
 				<div className="lightbox__wrapper">
-					<button className="lightbox__close" aria-label="Close lightbox">
+					<button type="button" className="lightbox__close" aria-label="Close lightbox" onClick={closeModal}>
 						<svg focusable="false" aria-hidden="true" width="17" height="17" fill="none">
 							<use xlinkHref="#close" />
 						</svg>
 					</button>
 				</div>
 				<div className="lightbox__photo">
-					<img src="/artPrints/2.jpg" alt="" className="lightbox__img" />
+					<img src={currentPrint} alt="Print example" className="lightbox__img" />
 				</div>
 				<div className="lightbox__btns">
-					<button className="lightbox__btn lightbox__download">
+					<a href={currentPrint} download="PrintExample.png" className="lightbox__btn lightbox__download">
 						<svg className="lightbox__svg" focusable="false" aria-hidden="true" width="25" height="21" fill="none">
 							<use xlinkHref="#download" />
 						</svg>
-						<a href="/artPrints/2.jpg" download="name.png">
-							Download
-						</a>
-					</button>
-					<button className="lightbox__btn lightbox__share">
+						Download
+					</a>
+					<button type="button" className="lightbox__btn lightbox__share">
 						<svg className="lightbox__svg" focusable="false" aria-hidden="true" width="25" height="21" fill="none">
 							<use xlinkHref="#share" />
 						</svg>
 						Share
 					</button>
-					<button className="btn btn--transparent lightbox__see">See in a frame</button>
-					{/* <button className="btn btn--white">Unlock photos</button> */}
+					<button type="button" className="btn btn--transparent lightbox__see">
+						See in a frame
+					</button>
+					{/* <button type="button" className="btn btn--white">Unlock photos</button> */}
 				</div>
 			</div>
 		</section>
