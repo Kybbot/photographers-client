52f51e58ab931a9b0a6017fca32c00caa7f5a39a
Артём Одийко <odiykoartem@gmail.com>
2022-09-16 18:34:36 +0300

    Updated overlay behavior

---
+++ src/components/Modal.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { onTab } from "../utils/onTab";
+
+type ModalProps = {
+	overlay: boolean;
+	active: boolean;
+	children: React.ReactNode;
+	closeModal: () => void;
+};
+
+export const Modal: React.FC<ModalProps> = ({ overlay, active, children, closeModal }) => {
+	const wrapperRef = React.useRef<HTMLDivElement>(null);
+
+	React.useEffect(() => {
+		let handleModalKeyboard: (event: KeyboardEvent) => void;
+
+		if (active) {
+			if (wrapperRef.current) {
+				const elems: NodeListOf<HTMLButtonElement & HTMLInputElement> =
+					wrapperRef.current.querySelectorAll("button, input, a");
+				const arrOfEllems = Array.from(elems);
+
+				for (const elem of elems) {
+					elem.style.display = "block";
+				}
+
+				handleModalKeyboard = onTab(wrapperRef, arrOfEllems, closeModal);
+
+				document.addEventListener("keydown", handleModalKeyboard);
+			}
+		} else {
+			if (wrapperRef.current) {
+				const elems: NodeListOf<HTMLButtonElement & HTMLInputElement> =
+					wrapperRef.current.querySelectorAll("button, input");
+
+				for (const elem of elems) {
+					elem.style.display = "none";
+				}
+			}
+		}
+
+		return () => document.removeEventListener("keydown", handleModalKeyboard);
+	}, [active, closeModal]);
+
+	React.useEffect(() => {
+		if (active) {
+			wrapperRef.current?.querySelector("button")?.focus();
+		}
+	}, [active]);
+
+	return (
+		<div
+			aria-hidden={!active}
+			className={`modal ${active ? "modal--visible" : ""}`}
+			role="dialog"
+			aria-modal="true"
+			aria-label="Modal window"
+		>
+			{overlay ? <div className="modal__overlay"></div> : null}
+			<div className="modal__wrapper" ref={wrapperRef}>
+				{children}
+			</div>
+		</div>
+	);
+};
