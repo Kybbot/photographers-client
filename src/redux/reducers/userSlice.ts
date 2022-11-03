900d524bf029c6d8f6e8fbd4a8a4a9988aa971e5
Артём Одийко <odiykoartem@gmail.com>
2022-11-03 17:47:05 +0200

    Added albums and photo data

--- src/redux/reducers/userSlice.ts
+++ src/redux/reducers/userSlice.ts
@@ -2,14 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 import { RootState } from "../store";
-import { UserResponse } from "../../@types/api";
+
+import { AlbumsResponse, PhotosResponse, UserResponse } from "../../@types/api";
 
 type UserStore = {
 	userData: UserResponse | null;
+	albums: AlbumsResponse;
+	photos: PhotosResponse;
 };
 
 const initialState: UserStore = {
 	userData: null,
+	albums: [],
+	photos: [],
 };
 
 export const userSlice = createSlice({
@@ -29,11 +34,20 @@ export const userSlice = createSlice({
 				state.userData.client_name = action.payload;
 			}
 		},
+		setUserAlbums: (state, action: PayloadAction<AlbumsResponse>) => {
+			state.albums = action.payload;
+		},
+		setUserPhotos: (state, action: PayloadAction<PhotosResponse>) => {
+			state.photos = action.payload;
+		},
 	},
 });
 
-export const { setUserData, changeUserSelfie, changeUserName } = userSlice.actions;
+export const { setUserData, changeUserSelfie, changeUserName, setUserAlbums, setUserPhotos } = userSlice.actions;
 
 export const getUserData = (state: RootState) => state.user.userData;
+export const getUserAlbumsLength = (state: RootState) => state.user.albums.length;
+export const getUserAlbums = (state: RootState) => state.user.albums;
+export const getUserPhotos = (state: RootState) => state.user.photos;
 
 export default userSlice.reducer;
