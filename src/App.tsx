199f88a9492214ba04e9a09b11cdb04daebd9880
Артём Одийко <odiykoartem@gmail.com>
2022-11-03 17:52:12 +0200

    Fetching user albums and photos

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ import Terms from "./pages/Terms";
 import Privacy from "./pages/Privacy";
 import NotFound from "./pages/NotFound";
 
-import { AuthRoutes } from "./components";
+import { AuthRoutes, Loader } from "./components";
 
 import { useAuthFetch } from "./hooks/useAuthFetch";
 import { useAppDispatch, useAppSelector } from "./hooks/reduxHooks";
 import { getIsLoggedIn } from "./redux/reducers/authSlice";
-import { getUserData, setUserData } from "./redux/reducers/userSlice";
+import { getUserData, setUserAlbums, setUserData, setUserPhotos } from "./redux/reducers/userSlice";
 
-import { UserResponse } from "./@types/api";
+import { AlbumsResponse, PhotosResponse, UserResponse } from "./@types/api";
 
 const App: React.FC = () => {
 	const isLoggedIn = useAppSelector(getIsLoggedIn);
@@ -23,24 +23,48 @@ const App: React.FC = () => {
 	const dispatch = useAppDispatch();
 	const userData = useAppSelector(getUserData);
 
-	const { error, request } = useAuthFetch();
+	const { loading: clientLoading, error: clientError, request: clientRequest } = useAuthFetch(true);
+	const { loading: albumsLoading, error: albumsError, request: albumsRequest } = useAuthFetch(true);
+	const { loading: photosLoading, error: photosError, request: photosRequest } = useAuthFetch(true);
 
 	useEffect(() => {
 		const getUserData = async () => {
-			const result = await request<UserResponse>("/client", "GET");
+			const result = await clientRequest<UserResponse>("/client", "GET");
 
 			if (result?.success) {
 				dispatch(setUserData(result.data));
 			}
 		};
 
+		const getUserAlbums = async () => {
+			const result = await albumsRequest<AlbumsResponse>("/albums", "GET");
+
+			if (result?.success) {
+				dispatch(setUserAlbums(result.data));
+			}
+		};
+
+		const getUserPhotos = async () => {
+			const result = await photosRequest<PhotosResponse>("/photos", "GET");
+
+			if (result?.success) {
+				dispatch(setUserPhotos(result.data));
+			}
+		};
+
 		if (!userData && isLoggedIn) {
 			void getUserData();
+			void getUserAlbums();
+			void getUserPhotos();
 		}
-	}, [isLoggedIn, userData, request, dispatch]);
+	}, [isLoggedIn, userData, clientRequest, albumsRequest, photosRequest, dispatch]);
 
-	if (error) {
-		return <p>{"An error has occurred: " + error}</p>;
+	if (clientLoading || albumsLoading || photosLoading) {
+		return <Loader />;
+	}
+
+	if (clientError || albumsError || photosError) {
+		return <p>An error has occurred: {clientError || albumsError || photosError}</p>;
 	}
 
 	if (isLoggedIn) {
