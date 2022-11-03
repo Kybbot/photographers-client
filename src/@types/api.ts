0aab0877429d5855b8de1d845f9618df80421835
Артём Одийко <odiykoartem@gmail.com>
2022-11-03 17:49:04 +0200

    Added new types

--- src/@types/api.ts
+++ src/@types/api.ts
@@ -23,6 +23,29 @@ export type NameResponse = {
 	selfie_image: string;
 };
 
+export type AlbumType = {
+	id: number;
+	person_id: string;
+	date: string;
+	album_name: string;
+	album_location: string;
+	album_logo: string;
+};
+
+export type AlbumsResponse = AlbumType[];
+
+export type PhotoType = {
+	id: number;
+	album_id: string;
+	client_name: string;
+	marked_url: string;
+	marked_logo: string;
+	photo_url: string;
+	photo_logo: string;
+};
+
+export type PhotosResponse = PhotoType[];
+
 export type ApiResult<T> = {
 	data: T;
 	success: true;
