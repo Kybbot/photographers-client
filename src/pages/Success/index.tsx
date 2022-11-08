4cb5e52c11a61082bb59ff1e56b2f6851511f9fa
Артём Одийко <odiykoartem@gmail.com>
2022-11-08 11:36:38 +0200

    Added user avatar

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,8 +1,13 @@
 import React, { FC } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
+import { useAppSelector } from "../../hooks/reduxHooks";
+import { getUserData } from "../../redux/reducers/userSlice";
+
 const Success: FC = () => {
 	const navigate = useNavigate();
+
+	const userData = useAppSelector(getUserData);
 
 	const albumData = JSON.parse(localStorage.getItem("AlbumData") || "") as {
 		albumId: number;
@@ -20,6 +25,16 @@ const Success: FC = () => {
 				<div className="header__container">
 					<Link className="header__link" to="/">
 						<img className="header__logo" src="/logo.svg" alt="PhotoDrop" width={125} height={16} />
+					</Link>
+					<Link to="/settings" className="header__avatar terms__avatar" aria-label="Settings">
+						<img
+							src={userData ? userData.selfie_image : "/avatar.png"}
+							alt="avatar"
+							aria-hidden="true"
+							className="header__img"
+							width={35}
+							height={35}
+						/>
 					</Link>
 				</div>
 			</header>
