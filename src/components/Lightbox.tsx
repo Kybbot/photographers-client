146d5fce5fa33f81b554e116d843b231281db541
Артём Одийко <odiykoartem@gmail.com>
2022-11-08 16:07:47 +0200

    Test

--- src/components/Lightbox.tsx
+++ src/components/Lightbox.tsx
@@ -65,7 +65,7 @@ export const Lightbox: FC<LightboxProps> = ({ currentPhoto, owned, closeModal, o
 			.then((blob) => {
 				const blobUrl = window.URL.createObjectURL(blob);
 				const a = document.createElement("a");
-				a.download = "image.jpg";
+				a.download = `${print.id}.${print.photo_url.split(".")[4]}`;
 				a.href = blobUrl;
 				document.body.appendChild(a);
 				a.click();
